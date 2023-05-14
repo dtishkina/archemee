b6ae6882a01f6e6c98fdfc1fc12ab2a7d76df438
dtishkina <lfhmztshkn@gmail.com>
2023-05-14 23:17:39 +0300

    add pregame screen

--- src/main/java/swing/SettingsScreen.java
+++ src/main/java/swing/SettingsScreen.java
@@ -4,12 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 
 class SettingsScreen extends JPanel {
-
-
     public SettingsScreen() {
+        setBackground(Color.WHITE);
+        setPreferredSize(new Dimension(1400, 700));
 
         setLayout(new GridBagLayout());
-
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = 0;
         constraints.gridy = 0;
@@ -36,17 +35,15 @@ class SettingsScreen extends JPanel {
 
         add(contentPane, constraints);
 
-        /////////////////////////////////////////////////////////////////////////////////////////////////////
-
         FlowLayout buttonLayout = new FlowLayout(FlowLayout.LEADING, 30, 0);
         JPanel buttonPanel = new JPanel(buttonLayout);
-
 
         JButton saveButton = new RoundButton("СОХРАНИТЬ");
         saveButton.setBorder(new RoundBorder(5, Color.decode("#3AAF37")));
         saveButton.setBackground(Color.decode("#3AAF37"));
         saveButton.setForeground(Color.WHITE);
         saveButton.setFont(new Font("Inter", Font.PLAIN, 20));
+        saveButton.setPreferredSize(new Dimension(165, 44));
 
         JButton shootingButton1 = new RoundButton("Перейти в меню настроек \"Альтернативная стрельба\"");
         shootingButton1.setFont(new Font("Inter", Font.PLAIN, 20));
@@ -54,6 +51,7 @@ class SettingsScreen extends JPanel {
         Color color = new Color(0x3AAF37);
         Color transparentColor = new Color(color.getRed(), color.getGreen(), color.getBlue(), (int) (255 * 0.13));
         shootingButton1.setBackground(transparentColor);
+        shootingButton1.setPreferredSize(new Dimension(573, 44));
 
         GridBagConstraints buttonConstraints = new GridBagConstraints();
         buttonConstraints.gridx = 0;
@@ -63,14 +61,9 @@ class SettingsScreen extends JPanel {
         buttonConstraints.fill = GridBagConstraints.HORIZONTAL;
 
         buttonPanel.add(saveButton);
-        saveButton.setPreferredSize(new Dimension(165, 44));
         buttonPanel.add(shootingButton1);
-        shootingButton1.setPreferredSize(new Dimension(573, 44));
         add(buttonPanel, buttonConstraints);
-
         buttonPanel.setBackground(Color.WHITE);
-
-        /////////////////////////////////////////////////////////////////////////////////////////////
 
         JPanel mainPane = new JPanel(new GridBagLayout());
         mainPane.setBorder(BorderFactory.createEmptyBorder(24, 10, 0, 200));
@@ -106,7 +99,7 @@ class SettingsScreen extends JPanel {
         c.insets = new Insets(5, 5, 0, 20);
         c.gridx = 0;
         c.gridy = 0;
-
+        panel1.setBackground(Color.decode("#F6F6F6"));
         mainPane.add(panel1, c);
 
         Font font = new Font("Inter", Font.PLAIN, 24);
@@ -115,27 +108,20 @@ class SettingsScreen extends JPanel {
         for (int i = 0; i < labels1.length; i++) {
             c.gridx = 0;
             JPanel template = new JPanel(new FlowLayout(FlowLayout.LEADING, 10, 5));
-
             template.setAlignmentX(Component.LEFT_ALIGNMENT);
             c.anchor = GridBagConstraints.WEST;
             c.insets = new Insets(0, 0, 10, 0);
             c.ipadx = 70;
             c.fill = GridBagConstraints.WEST;
-
             panel1.add(template, c);
-
             template.add(labels1[i], c);
             labels1[i].setFont(font);
-
             c.gridx = 1;
-
             template.add(text1[i], c);
             text1[i].setFont(inputFont);
             text1[i].setForeground(Color.decode("#828282"));
             text1[i].setBorder(new RoundBorder(5, Color.decode("#828282")));
-
             text1[i].setPreferredSize(new Dimension(84, 34));
-
             template.setBackground(Color.decode("#F6F6F6"));
             c.gridy++;
         }
@@ -144,6 +130,7 @@ class SettingsScreen extends JPanel {
         c.gridy = 0;
 
         JPanel panel2 = new JPanel(new GridLayout(0, 1, 10, 10));
+        panel2.setBackground(Color.decode("#F6F6F6"));
         mainPane.add(panel2, c);
 
         JLabel[] labels2 = {prepareTime, durationSeries, completionWarning, emptyLabel, emptyLabel1};
@@ -156,16 +143,13 @@ class SettingsScreen extends JPanel {
             template.setAlignmentX(Component.LEFT_ALIGNMENT);
             if (i < text2.length) {
                 panel2.add(template, c);
-
                 template.add(labels2[i]);
                 labels2[i].setFont(font);
-
                 template.add(text2[i]);
                 text2[i].setFont(inputFont);
                 text2[i].setForeground(Color.decode("#828282"));
                 text2[i].setColumns(5);
                 text2[i].setBorder(new RoundBorder(5, Color.decode("#828282")));
-
                 template.add(Box.createHorizontalStrut(1));
                 template.setBackground(Color.decode("#F6F6F6"));
                 c.fill = GridBagConstraints.HORIZONTAL;
@@ -182,19 +166,9 @@ class SettingsScreen extends JPanel {
         mainConstraints.gridx = 0;
         mainConstraints.gridy = 2;
         mainConstraints.insets = new Insets(47, 82, 75, 100);
-
         add(mainPane, mainConstraints);
-        setBorder(new RoundBorder(15, Color.WHITE));
-
-        panel1.setBackground(Color.decode("#F6F6F6"));
-        panel2.setBackground(Color.decode("#F6F6F6"));
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         shootingButton1.addActionListener(e -> {
-            // Создаем экземпляр класса AlternativeFireSettingsScreen и отображаем его на экране
             JFrame frame = (JFrame) SwingUtilities.getRoot(SettingsScreen.this);
             frame.getContentPane().removeAll();
             frame.getContentPane().add(new AltSettingsScreen());
@@ -205,7 +179,6 @@ class SettingsScreen extends JPanel {
             TimerBuilder timerBuilder = new TimerBuilder(playersNumber_, rotation_, testSeriesNumber_, targetingSeriesNumber_,
                     firstSeries_, prepareTime_, durationSeries_, completionWarning_);
             if (timerBuilder.isCorrect()) {
-                // Создаем экземпляр класса AlternativeFireSettingsScreen и отображаем его на экране
                 JFrame frame = (JFrame) SwingUtilities.getRoot(SettingsScreen.this);
                 frame.getContentPane().removeAll();
                 frame.getContentPane().add(new GameScreen(timerBuilder.build()));
@@ -214,10 +187,6 @@ class SettingsScreen extends JPanel {
             } else {
                 timerBuilder.applyHintsChanges();
             }
-
         });
-
-        setBackground(Color.WHITE);
-        setPreferredSize(new Dimension(1400, 700));
     }
 }
